35ec8ecda53bc76869085437e55b456420259b26
rohitvarshney-web <rohit.varshney@stampmyvisa.com>
2025-08-22 15:21:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,12 @@ import './globals.css';
 
 export const metadata = {
   title: 'SMV Logistics Console',
-  description: 'COnsole for Logistics Operations',
+  description: 'Internal logistics PWA',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/* Material Icons (outlined) */}
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
